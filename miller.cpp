c544adf8df89143852cdaca975deaeca5717b4b2
ocisra <nathanarsicault@gmail.com>
2021-03-14 18:46:28 +0100

    bottom status line is finished

--- miller.cpp
+++ miller.cpp
@@ -8,7 +8,29 @@
 #include <map>
 #include <string>
 
+#include <grp.h>
+#include <pwd.h>
+#include <sys/stat.h>
+
 Logger *log;
+
+
+static std::string formatBytes(long long bytes) {
+    const char *sizes[5] = {"B", "KB", "MB", "GB", "TB"};
+    std::string out;
+    char size[8];  // up to 9999TB ("9999.99\0")
+
+    int i;
+    long double dblByte = bytes;
+    for (i = 0; i < 5 && bytes >= 1024; i++, bytes /= 1024)
+        dblByte = bytes / 1024.0;
+
+    snprintf(size, 8, "%.2Lf", dblByte);
+    out += size;
+    out += sizes[i];
+
+    return out;
+}
 
 
 /**
@@ -64,11 +86,12 @@ Miller::Miller(unsigned int scrolloff, fs::path start_path) {
     int maxx = getmaxx(stdscr);
     int maxy = getmaxy(stdscr);
 
-    panelLeft   = new Window;
-    panelMiddle = new Window;
-    panelRight  = new Window;
-    topLine     = new Window;
-    bottomLine  = new Window;
+    panelLeft       = new Window;
+    panelMiddle     = new Window;
+    panelRight      = new Window;
+    topLine         = new Window;
+    bottomLeftLine  = new Window;
+    bottomRightLine = new Window;
 
     auto setWindow = [](Window *win, int sizex, int sizey, int startx, int starty,
                         int posx, int posy, int vsizex, int vsizey) {
@@ -93,7 +116,8 @@ Miller::Miller(unsigned int scrolloff, fs::path start_path) {
     setWindow(right(), maxx / 2, path()->child()->numOfEntries(), 0, 0, maxx / 2,
               0 + 1 /*top bar*/, maxx / 2, maxy - 1 - 1 /*status bar*/ - 1 /*top bar*/);
     setWindow(top(), maxx, 1, 0, 0, 0, 0, maxx, 1);
-    setWindow(bottom(), maxx, 1, 0, 0, 0, maxy - 1, maxx, 1);
+    setWindow(bottomLeft(), maxx / 2, 1, 0, 0, 0, maxy - 1, maxx / 2, 1);
+    setWindow(bottomRight(), maxx / 2, 1, 0, 0, maxx / 2, maxy - 1, maxx / 2, 1);
     // clang-format on
 
     left()->setLine(0);
@@ -106,12 +130,15 @@ Miller::Miller(unsigned int scrolloff, fs::path start_path) {
         setScrolloff((maxy - 1) / 2);
 
     draw();
+    updateFileStatus();
+    updateDirStatus();
 
     log->debug(left(), "Init left");
     log->debug(middle(), " Init middle");
     log->debug(right(), "Init right");
     log->debug(top(), "Init top");
-    log->debug(bottom(), "Init bottom");
+    log->debug(bottomLeft(), "Init bottom");
+    log->debug(bottomRight(), "Init bottom");
 }
 
 Miller::~Miller() {
@@ -120,14 +147,16 @@ Miller::~Miller() {
     delwin(middle()->win);
     delwin(right()->win);
     delwin(top()->win);
-    delwin(bottom()->win);
+    delwin(bottomLeft()->win);
+    delwin(bottomRight()->win);
 
     // Delete the panels
     delete left();
     delete middle();
     delete right();
     delete top();
-    delete bottom();
+    delete bottomLeft();
+    delete bottomRight();
 
     delete path();
 }
@@ -181,23 +210,110 @@ Colors Miller::getCurrentFileColor() {
  * Update the file status information in the bottom window
  */
 void Miller::updateFileStatus() {
-    wclear(bottom()->win);
-    std::string status;
-    fs::perms p = fs::status(path()->current()->getFileByLine(middle()->line())->path).permissions();
-    status += ((p & fs::perms::owner_read) != fs::perms::none ? 'r' : '-');
-    status += ((p & fs::perms::owner_write) != fs::perms::none ? 'w' : '-');
-    status += ((p & fs::perms::owner_exec) != fs::perms::none ? 'x' : '-');
-    status += ((p & fs::perms::group_read) != fs::perms::none ? 'r' : '-');
-    status += ((p & fs::perms::group_write) != fs::perms::none ? 'w' : '-');
-    status += ((p & fs::perms::group_exec) != fs::perms::none ? 'x' : '-');
-    status += ((p & fs::perms::others_read) != fs::perms::none ? 'r' : '-');
-    status += ((p & fs::perms::others_write) != fs::perms::none ? 'w' : '-');
-    status += ((p & fs::perms::others_exec) != fs::perms::none ? 'x' : '-');
-    bottom()->noWrapOutput(status);
-    prefresh(bottom()->win, bottom()->starty, bottom()->startx, bottom()->posy, bottom()->posx,
-             bottom()->vsizey + bottom()->posy, bottom()->vsizex + bottom()->posx);
+    wclear(bottomLeft()->win);
+    fs::path file = path()->current()->getFileByLine(middle()->line())->path;
+    std::string output;
+
+    // permissions
+    fs::perms p = fs::status(file).permissions();
+    output += ((p & fs::perms::owner_read) != fs::perms::none ? 'r' : '-');
+    output += ((p & fs::perms::owner_write) != fs::perms::none ? 'w' : '-');
+    output += ((p & fs::perms::owner_exec) != fs::perms::none ? 'x' : '-');
+    output += ((p & fs::perms::group_read) != fs::perms::none ? 'r' : '-');
+    output += ((p & fs::perms::group_write) != fs::perms::none ? 'w' : '-');
+    output += ((p & fs::perms::group_exec) != fs::perms::none ? 'x' : '-');
+    output += ((p & fs::perms::others_read) != fs::perms::none ? 'r' : '-');
+    output += ((p & fs::perms::others_write) != fs::perms::none ? 'w' : '-');
+    output += ((p & fs::perms::others_exec) != fs::perms::none ? 'x' : '-');
+
+    output += ' ';
+
+    struct stat sb;
+    if (stat(file.c_str(), &sb) == -1) {
+        log->warning("stat() returned -1 when updating the file status");
+        return;
+    }
+
+    // link count
+    output += std::to_string(sb.st_nlink);
+    output += ' ';
+
+    // ownership user
+    struct passwd *pwd = new passwd;
+    if ((pwd = getpwuid(sb.st_uid)) == NULL) {
+        log->warning("getpwuid() returned NULL struct when updating file status");
+        return;
+    }
+    output += pwd->pw_name;
+    output += ' ';
+
+    // ownership group
+    struct group *grp = new group;
+    if ((grp = getgrgid(sb.st_gid)) == NULL) {
+        log->warning("getgrgid() returned NULL struct when updating file status");
+        return;
+    }
+    output += grp->gr_name;
+    output += ' ';
+
+    // number of entries if dir, size else
+    if (fs::is_directory(file))
+        output += std::to_string(path()->child()->numOfEntries());
+    else
+        output += formatBytes(sb.st_size);
+    output += ' ';
+
+    // last change time
+    char time[17];
+    strftime(time, 17, "%F %R", localtime(&sb.st_ctime));
+    output += time;
+    output += ' ';
+
+
+    bottomLeft()->noWrapOutput(output);
+    prefresh(bottomLeft()->win, bottomLeft()->starty, bottomLeft()->startx,
+             bottomLeft()->posy, bottomLeft()->posx, bottomLeft()->vsizey + bottomLeft()->posy,
+             bottomLeft()->vsizex + bottomLeft()->posx);
 }
 
+/**
+ * Update the folder status information in the bottom window
+ */
+void Miller::updateDirStatus() {
+    wclear(bottomRight()->win);
+    fs::path dir = path()->path();
+    std::string output;
+    std::string paddedOutput;  // output but padded with spaces to right align
+
+    struct stat sb;
+    if (stat(dir.c_str(), &sb) == -1) {
+        log->warning("stat() returned -1 when updating the file status");
+        return;
+    }
+
+
+
+    // free space in filesystem
+    output += formatBytes(fs::space(dir).available);
+    output += " free  ";
+
+    // position in dir
+    output += std::to_string(middle()->line() + 1);
+    output += '/';
+    output += std::to_string(path()->current()->numOfEntries());
+
+
+    int spaces = bottomRight()->sizex - output.size();
+    for (int i = 0; i < spaces; i++)
+        paddedOutput += ' ';
+    paddedOutput += output;
+
+    bottomRight()->noWrapOutput(paddedOutput);
+    prefresh(bottomRight()->win, bottomRight()->starty, bottomRight()->startx,
+             bottomRight()->posy, bottomRight()->posx,
+             bottomRight()->vsizey + bottomRight()->posy,
+             bottomRight()->vsizex + bottomRight()->posx);
+}
 
 /**
  * Draw the windows on the screen
@@ -216,15 +332,13 @@ void Miller::draw() {
     // box(middle()->win, 0, 0);
     // box(right()->win, 0, 0);
     // box(top()->win, 0, 0);
-    // box(bottom()->win, 0, 0);
-
-    // middle()->setLine(0);
+    // box(bottomLeft()->win, 0, 0);
+    // box(bottomRight()->win, 0, 0);
 
     left()->display(path()->parent());
     middle()->display(path()->current());
     right()->display(path()->child());
     updateWD();
-    updateFileStatus();
 
 
     prefresh(left()->win, left()->starty, left()->startx, left()->posy, left()->posx,
@@ -235,8 +349,14 @@ void Miller::draw() {
              right()->vsizey + right()->posy, right()->vsizex + right()->posx);
     prefresh(top()->win, top()->starty, top()->startx, top()->posy, top()->posx,
              top()->vsizey + top()->posy, top()->vsizex + top()->posx);
-    prefresh(bottom()->win, bottom()->starty, bottom()->startx, bottom()->posy, bottom()->posx,
-             bottom()->vsizey + bottom()->posy, bottom()->vsizex + bottom()->posx);
+    prefresh(bottomLeft()->win, bottomLeft()->starty, bottomLeft()->startx,
+             bottomLeft()->posy, bottomLeft()->posx, bottomLeft()->vsizey + bottomLeft()->posy,
+             bottomLeft()->vsizex + bottomLeft()->posx);
+    prefresh(bottomRight()->win, bottomRight()->starty, bottomRight()->startx,
+             bottomRight()->posy, bottomRight()->posx,
+             bottomRight()->vsizey + bottomRight()->posy,
+             bottomRight()->vsizex + bottomRight()->posx);
+
 
     wmove(middle()->win, middle()->line(), 0);
     middle()->attr_line(SELECTED);
@@ -264,7 +384,8 @@ void Miller::resizeTerm() {
     setWindow(right(), maxx / 2, 0 + 1 /*top bar*/, maxx / 2,
             maxy - 1 - 1 /*top bar*/ - 1 /*status bar*/);
     setWindow(top(), 0, 0, maxx, 1);
-    setWindow(bottom(), 0, maxy - 1, maxx, 1);
+    setWindow(bottomLeft(), 0, maxy - 1, maxx / 2, 1);
+    setWindow(bottomRight(), maxx / 2, maxy - 1, maxx / 2, 1);
     // clang-format on
 
     // Set scrolloff accordig to the size of the window
@@ -286,14 +407,20 @@ void Miller::resizeTerm() {
              right()->vsizey + right()->posy, right()->vsizex + right()->posx);
     prefresh(top()->win, top()->starty, top()->startx, top()->posy, top()->posx,
              top()->vsizey + top()->posy, top()->vsizex + top()->posx);
-    prefresh(bottom()->win, bottom()->starty, bottom()->startx, bottom()->posy, bottom()->posx,
-             bottom()->vsizey + bottom()->posy, bottom()->vsizex + bottom()->posx);
+    prefresh(bottomLeft()->win, bottomLeft()->starty, bottomLeft()->startx,
+             bottomLeft()->posy, bottomLeft()->posx, bottomLeft()->vsizey + bottomLeft()->posy,
+             bottomLeft()->vsizex + bottomLeft()->posx);
+    prefresh(bottomRight()->win, bottomRight()->starty, bottomRight()->startx,
+             bottomRight()->posy, bottomRight()->posx,
+             bottomRight()->vsizey + bottomRight()->posy,
+             bottomRight()->vsizex + bottomRight()->posx);
 
     log->debug(left(), "Resize left");
     log->debug(middle(), "Resize middle");
     log->debug(right(), "Resize right");
     log->debug(top(), "Resize top");
-    log->debug(bottom(), "Resize bottom");
+    log->debug(bottomLeft(), "Resize bottom");
+    log->debug(bottomRight(), "Resize bottom");
 }
 
 /**
@@ -372,9 +499,6 @@ void Miller::move(Direction direction) {
 
         middle()->setLine(0);
 
-        // if (fs::is_empty(path()->path()))
-        // path()->current()->addVirtual("empty");
-        // else
         path()->previewChild(right());
 
         // resize the pad
@@ -386,6 +510,7 @@ void Miller::move(Direction direction) {
     } break;
     }
     updateFileStatus();
+    updateDirStatus();
 }
 
 Miller *miller;
--- miller.hpp
+++ miller.hpp
@@ -96,7 +96,8 @@ class Miller {
     inline Window *middle() { return panelMiddle; }
     inline Window *right() { return panelRight; }
     inline Window *top() { return topLine; }
-    inline Window *bottom() { return bottomLine; }
+    inline Window *bottomLeft() { return bottomLeftLine; }
+    inline Window *bottomRight() { return bottomRightLine; }
 
     inline bool isAtTopOfWindow() {
         return middle()->line() <= middle()->starty + scrolloff();
@@ -116,8 +117,12 @@ class Miller {
     inline void setPath(Path *p) { currentPath = p; }
 
     private:
-    inline void updateWD() { wclear(top()->win); top()->noWrapOutput(path()->path().string()); }
-    inline void updateFileStatus();
+    inline void updateWD() {
+        wclear(top()->win);
+        top()->noWrapOutput(path()->path().string());
+    }
+    void updateFileStatus();
+    void updateDirStatus();
     unsigned int wantsScrolloff;  // the wanted scrolloff
     // when screen is too small, it might be reduced so the cursor can be visible giving:
     unsigned int scrolloffLines;  // lines to keep below and above cursor
@@ -125,7 +130,8 @@ class Miller {
     Window *panelMiddle;
     Window *panelRight;
     Window *topLine;
-    Window *bottomLine;
+    Window *bottomLeftLine;
+    Window *bottomRightLine;
     Path *currentPath;
 };
 
